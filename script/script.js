33474aa9e435ab3e964f487fd984b4996b8a8bae
100472007 <100472007@alumnos.uc3m.es>
2023-12-05 12:32:47 +0100

    cantidades de pedido y estilo

--- script/script.js
+++ script/script.js
@@ -4,15 +4,11 @@ var codPostal = 28013  // codigo postal del restaurante
 document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("buscarDir").addEventListener("click", function() {
-        checkPostalCode();
-    });
-
-    function checkPostalCode() {
         var postalCode = document.getElementById("postalCodeInput").value;
         if (postalCode == codPostal) {
-            window.location.href = "pages/a_domicilio/a-domicilio2.html";
+            window.location.href = "a-domicilio-2.html";
         } else {
             window.location.href = "error.html";
         }
-    };
+    });
 });
