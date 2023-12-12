9ddbaf3666a9680a188e86f496919e5f82626ee5
NataaNK <95182445+NataaNK@users.noreply.github.com>
2023-12-12 12:22:54 +0100

    Menu hamb casi, se mueve todo hacia arriba. deberia quedarse quieto

--- script/script.js
+++ script/script.js
@@ -75,25 +75,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // Desplegar menu de hamburguesa
 function toggleClass(elem,className){
 	if (elem.className.indexOf(className) !== -1){
-		elem.className = elem.className.replace(className,'');     
+		elem.className = elem.className.replace(className,'');    
 	}
 	else{
 		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
 	}
 	return elem;
 }
-
-function toggleDisplay(elem){
-	const curDisplayStyle = elem.style.display;			
-				
-	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-		elem.style.display = 'block';
-	}
-	else{
-		elem.style.display = 'none';
-	}
-}
-
 
 function toggleMenuDisplay(e){
 	const dropdown = e.currentTarget.parentNode;
